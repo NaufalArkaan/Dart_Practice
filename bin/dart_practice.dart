5a775b16712f9dbf676e2e9bde39c5c632b8ee4d
NaufalArkaan <naufalarkaan25@gmail.com>
2025-04-26 09:17:46 +0700

    new dart 3

--- bin/dart_practice.dart
+++ bin/dart_practice.dart
@@ -1,15 +1,59 @@
 import 'package:dart_practice/dart_practice.dart' as dart_practice;
+import 'dart:io';
 
 // void main(){
 //   var halo = "hallo";
 //   print(halo);
 // }
 
-const num pi = 3.14;
+// const num pi = 3.14;
+//
+// void main(){
+//   var radius = 7;
+//   print('Luas lingkaran dengan radius $radius = ${calculateCircleArea(radius)}');
+// }
+//
+// num calculateCircleArea(num radius) => pi * radius * radius;
 
-void main(){
-  var radius = 7;
-  print('Luas lingkaran dengan radius $radius = ${calculateCircleArea(radius)}');
+// void main(){
+//   var x;
+//   x = 7;
+//   x = "Hello, World!";
+//
+//   print(x);
+// }
+
+// void main(){
+//   stdout.write('Masukan nama: ');
+//   String name = stdin.readLineSync()!;
+//   stdout.write('Masukan usia: ');
+//   String usia = stdin.readLineSync()!;
+//   print("Halo nama saya $name, dan usia saya $usia tahun.");
+// }
+
+// void main(){
+//   var elevan = int.parse('11');
+//   print(elevan.runtimeType);
+//
+//   var twelve = double.parse('12.5');
+//   print(twelve.runtimeType);
+// }
+
+// void main() {
+//   print('"I think it\'s great!" I answered confidently');  //(\) escape string
+// }
+
+// void main() {
+//   print("Windows path: C:\\Program Files\\Dart");
+// }
+
+// void main() {
+//   print(r'Dia baru saja membeli komputer seharga $1,000.00');
+// }
+
+void main() {
+  print('Hi \u2665');
 }
 
-num calculateCircleArea(num radius) => pi * radius * radius;
+/// Output:
+/// Hi ♥
