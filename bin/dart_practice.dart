03955e8e97a31b666b00affba7f3af5b61cfe710
NaufalArkaan <naufalarkaan25@gmail.com>
2025-05-01 20:20:30 +0700

    collections dart

--- bin/dart_practice.dart
+++ bin/dart_practice.dart
@@ -168,8 +168,31 @@ import 'dart:io';
 
 
 //for loop
+// void main(){
+//   for(int i = 0; i < 10; i++){
+//     print(i);
+//   }
+// }
+
+
+//break and continue
+// void main(){
+//   var primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 73, 79, 83, 89, 97];
+//   var searchNumber = 13;
+//   print('Bilangan prima 1 - 100 adalah $searchNumber');
+//
+//   for(int i = 0; i < primeNumbers.length; i++){
+//     if(searchNumber == primeNumbers[i]){
+//       print('$searchNumber adalah bilangan prima ke-${i + 1}');
+//       break;
+//     }
+//     print('$searchNumber != ${primeNumbers[i]}');
+//   }
+// }
+
+
+//collections
 void main(){
-  for(int i = 0; i < 10; i++){
-    print(i);
-  }
+  List dynamictList = [1, "Dicoding", true];
+  print(dynamictList[1]);
 }
